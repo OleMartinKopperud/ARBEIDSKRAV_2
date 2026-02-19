6c6ed3b12bdadc5e6f90bdab5ce4ac10bfe31377
Ole Martin Hansen Kopperud <olemha@hiof.no>
2026-02-19 14:47:59 +0100

    Started

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import AddFrom from './AddFrom'
+import ShoppingList from './ShoppingList'
+
+export default function App() {
+  const [items, setItems] = useState([
+    {id: 2, name: 'Egg', quantity: 1, isBought: true },
+    {id: 1, name: 'Melk', quantity: 2, isBought: false },
+  ])
+
+  const handleAddItem = (newItem) => {
+    setItems([newItem, ...items])
+  }
+
+  const handleUpdateItem = (updatedItem) => {
+    setItems(items.map(item => item.id === updatedItem.id ? updatedItem : item))
+  }
+
+  return (
+    <main className='app-container'>
+      <section className='shopping-card'>
+        <h1>Handleliste</h1>
+        <AddForm onAddItem={handleAddItem} />
+        <ShoppingList items={items} onUpdateItem={handleUpdateItem} />
+      </section>
+    </main>
+  )
+}
